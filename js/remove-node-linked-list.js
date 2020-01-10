177e3cf82b4d45581589d8a816b638dddb7bf926
901david <vdavidhammond@gmail.com>
2020-01-10 09:05:42 -0700

    added in alg description

--- js/remove-node-linked-list.js
+++ js/remove-node-linked-list.js
@@ -1,3 +1,5 @@
+/* Given a node from a Linked List remove the node from the list assuming it is not the tail or the head */
+
 const removeNodeFromList = node => {
   if (node && node.next && node.next.next) {
     node.data = node.next.data;
