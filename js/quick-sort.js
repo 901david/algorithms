6dd7bcc643cf21eb4f30f4ac7d9a166315c4a51f
901david <vdavidhammond@gmail.com>
2020-02-13 11:05:18 -0700

    implemented quick sort with pivot helper

--- js/quick-sort.js
+++ js/quick-sort.js
@@ -1,16 +1,30 @@
-const pivot = (arr, start, end, pivotIdx) => {
+const pivot = (arr, start, end, pivotIdx = start) => {
   if (pivotIdx !== start) {
     [arr[start], arr[pivotIdx]] = [arr[pivotIdx], arr[start]];
   }
-  const pivotVal = arr[pivotIdx];
-  let finalPivotIDX = 0;
+  const pivotVal = arr[start];
+  let finalPivotIDX = start;
   for (let i = start + 1; i <= end; i++) {
     if (arr[i] <= pivotVal) {
       finalPivotIDX++;
+      [arr[i], arr[finalPivotIDX]] = [arr[finalPivotIDX], arr[i]];
     }
   }
+  [arr[start], arr[finalPivotIDX]] = [arr[finalPivotIDX], arr[start]];
+  return finalPivotIDX;
 };
 
-const quickSort = arr => {};
+const getRandomValueBetween = (start, end) =>
+  Math.floor(Math.random() * end + start);
 
-module.exports = { pivot, quickSort };
+const quickSort = (arr, start = 0, end = arr.length - 1) => {
+  if (start < end) {
+    const pivotIdx = pivot(arr, start, end, getRandomValueBetween(start, end));
+    quickSort(arr, start, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, end);
+  }
+
+  return arr;
+};
+
+module.exports = { pivot, quickSort, getRandomValueBetween };
--- js/quick-sort.test.js
+++ js/quick-sort.test.js
@@ -1,21 +1,45 @@
-const { quickSort, pivot } = require("./quick-sort");
+const { quickSort, pivot, getRandomValueBetween } = require("./quick-sort");
 
 describe("Quick Sort Alg", () => {
-  test.skip("should sort correctly", () => {
+  test("should sort correctly", () => {
     const sorted = quickSort([3, 5, 1, 2, 4]);
-    expeced(sorted).toEqual([1, 2, 3, 4, 5]);
+    expect(sorted).toEqual([1, 2, 3, 4, 5]);
+  });
+});
+
+describe("getRandomValueBetween", () => {
+  test("should return random value between correct range", () => {
+    const val = getRandomValueBetween(1, 10);
+    console.log(val);
+    expect(val >= 1 && val <= 10).toEqual(true);
   });
 });
 
 describe("Pivot method", () => {
-  test.skip("should Return array in correct order", () => {});
+  test("should Return array in correct order", () => {
+    const testData = [4, 3, 6, 2, 8];
+    const pivotIdx = pivot(testData, 0, testData.length - 1, 2);
 
-  test.skip("should Return correct index", () => {
+    expect(pivotIdx).toEqual(3);
+    testData.slice(0, pivotIdx).map(val => expect(val < 6));
+    testData.slice(pivotIdx).map(val => expect(val > 6));
+  });
+
+  test("should Return correct index", () => {
     const testData = [4, 3, 6, 2, 8];
     const pivotIdx = pivot(testData, 0, testData.length - 1, 2);
 
     expect(pivotIdx).toEqual(3);
     expect(testData.slice(0, pivotIdx).length).toEqual(3);
     expect(testData[testData.length - 1]).toEqual(8);
+  });
+
+  test("should Return correct index", () => {
+    const testData = [4, 3, 9, 10, 3, 5, 6, 2, 8];
+    const pivotIdx = pivot(testData, 0, testData.length - 1, 6);
+
+    expect(pivotIdx).toEqual(5);
+    testData.slice(0, pivotIdx).map(val => expect(val < 6));
+    testData.slice(pivotIdx).map(val => expect(val > 6));
   });
 });
